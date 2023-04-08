e25020660d8d8e119a4f4f8f56aeca57d4c5c941
Magistry <Magistrigusti@yandex.ru>
2023-04-08 07:05:12 +0300

    1.6

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
 const btnDarkMode = document.querySelector('.dark-mode-btn');
 
-if (localStorage.getItem('darkMode') === 'dark') {
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
   btnDarkMode.classList.add('dark-mode-btn--active');
   document.body.classList.add('dark');
 }
+
+if (localStorage.getItem('darkMode') === 'dark') {
+  btnDarkMode.classList.add('dark-mode-btn--active');
+  document.body.classList.add('dark');
+} else if (localStorage.getItem('darkMode') === 'light') {
+  btnDarkMode.classList.remove('dark=mode-btn--active');
+  document.body.classList.remove('dark');
+}
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener('change', (event) => {
+  const newColorScheme = event.matches ? 'dark': 'light';
+
+  if (newColorScheme === 'dark') {
+    btnDarkMode.classList.add('dark-mode-btn--active');
+    document.body.classList.add('dark');
+    localStorage.setItem('darkMode', 'dark');
+  } else {
+    btnDarkMode.classList.add('dark-mode-btn--active');
+    document.body.classList.add('dark');
+  }
+})
 
 btnDarkMode.onclick = function() {
   btnDarkMode.classList.toggle('dark-mode-btn--active');
