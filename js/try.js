c46d1cbcab5bba7d90b6f48f6e740caddaec3eba
Magistry <Magistrigusti@yandex.ru>
2023-04-08 07:04:59 +0300

    1.5

--- js/try.js
+++ js/try.js
@@ -1,17 +1,11 @@
 const btnDarkMode = document.querySelector('.dark-mode-btn');
 
-if (localStorage.getItem('darkMode') === 'dark') {
+if (windows.matchMedia && windows.matchMedia("(preers-color-scheme: 'dark')").matches) {
   btnDarkMode.classList.add('dark-mode-btn--active');
   document.body.classList.add('dark');
 }
 
-btnDarkMode.onclick = function () {
-  btnDarkMode.classList.toggle('dark-mode-btn--active');
-  const isDark = document.body.classList.toggle('dark');
-
-  if (isDark) {
-    localStorage.setItem('darkMode', 'dark');
-  } else {
-    localStorage.setItem('darkMode', 'light');
-  }
-}
+if (localstorage.getItem('darkMode') === 'dark') {
+  btnDarkMode.classList.add('dark-mode-btn--active');
+  document.body.classList.add('dark');
+} else if ()
